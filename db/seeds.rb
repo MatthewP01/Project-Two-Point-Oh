a85e255268e37907a2bd0a6c9e583e716caec5ad
XRuiz9 <xdruiz92@gmail.com>
2019-07-02 14:13:24 -0400

    Added Images and placeholders

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,22 +8,23 @@
 Fish.destroy_all
 
 Fish.create(name: "Xavierfish", cost: 50)
-Fish.create(name: "Richardfish", cost: 75)
-Fish.create(name: "Roxannefish", cost: 100)
 Fish.create(name: "Peterfish", cost: 125)
-
-
-Fish.create(name: "Mattfish", cost: 150)
-Fish.create(name: "Echofish", cost: 175)
 Fish.create(name: "Maxfish", cost: 200)
-Fish.create(name: "Elijahfish", cost: 225)
-
-Fish.create(name: "Jennafish", cost: 250)
 Fish.create(name: "Ganeshfish", cost: 275)
+Fish.create(name: "Gigifish", cost: 500)
+
+Fish.create(name: "Richardfish", cost: 75)
+Fish.create(name: "Mattfish", cost: 150)
+Fish.create(name: "Elijahfish", cost: 225)
 Fish.create(name: "Leoniefish", cost: 300)
+Fish.create(name: "Princefish", cost: 500)
+
+Fish.create(name: "Roxannefish", cost: 100)
+Fish.create(name: "Echofish", cost: 175)
+Fish.create(name: "Jennafish", cost: 250)
 Fish.create(name: "Rinafish", cost: 325)
 
-Fish.create(name: "Gigifish", cost: 500)
-Fish.create(name: "Princefish", cost: 500)
+
+
 
 puts "Seed Complete!"
