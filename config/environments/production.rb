fc8b07af98d7218cb83f35e941f9b72893b97905
XRuiz9 <xdruiz92@gmail.com>
2019-07-05 11:49:37 -0400

    idk4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -79,8 +79,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # config.serve_static_assets = true
-  # config.assets.compile = true
+  config.serve_static_assets = true
+  config.assets.compile = true
 
 
   # Use a different logger for distributed setups.
